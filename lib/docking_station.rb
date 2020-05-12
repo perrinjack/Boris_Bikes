354f69d4f98da18cf192366513f675fe1fcc809d
Anthony <a.a.donovan@outlook.com>
2020-05-12 16:38:05 +0100

    Added dock method to to docking_station.rb and a corresponding test to docking_station_spec.rb

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,16 @@
-require './bike.rb'
+require 'bike'
+
+
 
 class DockingStation
+  attr_reader :bike
+  
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,8 @@
 require 'docking_station.rb'
+require 'bike.rb'
 
 describe DockingStation do
+  let(:docking_instance) {DockingStation.new}
 =begin
   it 'responds to the method release_bike' do
     docking_instance = DockingStation.new
@@ -9,5 +11,20 @@ describe DockingStation do
 =end
 
   it { is_expected.to respond_to :release_bike}
+
+  it 'gets a bike and expects bike to be working' do
+    #docking_instance = DockingStation.new
+    expect(docking_instance.release_bike.working?).to eq true
+  end
+
+  it { is_expected.to respond_to :dock}
+
+  it 'take a bike as an argument and allow access to the bike using the .bike method' do
+    #docking_instance = DockingStation.new
+    bike_instance = Bike.new
+    docking_instance.dock(bike_instance)
+    expect(docking_instance.bike).to eq bike_instance
+  end
+
 
 end

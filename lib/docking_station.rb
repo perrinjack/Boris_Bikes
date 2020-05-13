af551d34fbb87c4abc6f12318dfd5ae6673234b1
Sophie <sophiebrown438@gmail.com>
2020-05-13 15:23:53 +0100

    Refactor of Docking Station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,13 @@
-require 'bike'
-
-
+require './bike.rb'
 
 class DockingStation
   attr_reader :bike
-  
-  def release_bike
-    Bike.new
-  end
 
   def dock(bike)
     @bike = bike
   end
 
+  def release_bike
+    Bike.new
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,21 +10,20 @@ describe DockingStation do
   end
 =end
 
-  it { is_expected.to respond_to :release_bike}
+  it { is_expected.to respond_to :bike}
 
-  it 'gets a bike and expects bike to be working' do
+  it 'should gets a bike and expects bike to be working' do
     #docking_instance = DockingStation.new
     expect(docking_instance.release_bike.working?).to eq true
   end
 
   it { is_expected.to respond_to :dock}
 
-  it 'take a bike as an argument and allow access to the bike using the .bike method' do
+  it 'should take a bike as an argument and allow access to the bike using the .bike method' do
     #docking_instance = DockingStation.new
     bike_instance = Bike.new
-    docking_instance.dock(bike_instance)
-    expect(docking_instance.bike).to eq bike_instance
+    # docking_instance.dock(bike_instance)
+    # expect(docking_instance.bike).to eq bike_instance
+    expect(docking_instance.dock(bike_instance)).to eq bike_instance
   end
-
-
 end

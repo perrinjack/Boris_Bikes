88de8f9bd15e42e476c19020c68c31a32e285b8c
Sophie <sophiebrown438@gmail.com>
2020-05-13 17:53:09 +0100

    Rspec & reworking methods for max capacity of station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,22 +4,30 @@ require 'bike.rb'
 describe DockingStation do
   let(:docking_instance) {DockingStation.new}
 
-  it { is_expected.to respond_to :bike}
+  it { is_expected.to respond_to :bikes}
 
   describe '#release_bike' do
     it 'releases a bike' do
-      bike = Bike.new
-      docking_instance.dock(bike)
+      bikes = Bike.new
+      docking_instance.dock(bikes)
       #release docked bike
-      expect(docking_instance.release_bike).to eq bike
+      expect(docking_instance.release_bike).to eq bikes
     end
 
     it 'raises error when no bikes are available' do
-      expect{ docking_instance.release_bike }.to raise_error "No bikes, no worries"
+      expect{ docking_instance.release_bike }.to raise_error "No bikes available"
     end
   end
 
+  describe '#dock' do
   it { is_expected.to respond_to :dock}
+
+  it 'raises error when no spaces are available' do
+    20.times {docking_instance.dock Bike.new}
+    expect{ docking_instance.dock Bike.new }.to raise_error "No spaces to dock"
+  end
+end
+  
 
   # it 'should take a bike as an argument and allow access to the bike using the .bike method' do
   #   #docking_instance = DockingStation.new
